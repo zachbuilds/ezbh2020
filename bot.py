ce06612c848190433b6bf5851bf42394111e08bf
Zach Vincent <vincent_z@delbarton.org>
2020-04-20 15:14:48 -0400

    added basic defensive pawns

--- bot.py
+++ bot.py
@@ -24,11 +24,15 @@ def check_space_wrapper(r, c, board_size):
     except:
         return None
 
+
+
+
 def turn():
     """
     MUST be defined for robot to run
     This function will be called at the beginning of every turn and should contain the bulk of your robot commands
     """
+
     dlog('Starting Turn!')
     board_size = get_board_size()
 
@@ -79,28 +83,47 @@ def turn():
         # ^ I think this is related to the potential ambiguity of what the following else is referring to?
 
     else:
+        
+        # added this boolean so we don't waste bytecode on spawn loops if something's already been spawned
+        hasSpawned = False
+
         if team == Team.WHITE:
             index = 0
         else:
             index = board_size - 1
 
-
-        for _ in range(board_size):
-            if check_space(index, 7) == False:
-                 elif check_space(index, 8) == False:
-                     spawn(index, 8)
-                     dlog('Spawned unit at: (' + str(index) + ', ' + str(8) + ')')
-                spawn(index, 7)
-                dlog('Spawned unit at: (' + str(index) + ', ' + str(7) + ')')
-            else:
-                i = random.randint(0, board_size - 1)
-                if not check_space(index, i):
-                    spawn(index, i)
-                    dlog('Spawned unit at: (' + str(index) + ', ' + str(i) + ')')
+        # checks the third row from the bottom to see if there are enemy pawns and spawns defensive pawn on adjacent column
+        for col in range(0, 16):
+            if check_space(13, col) == opp_team:
+                if not check_space(index, col + 1):
+                    spawn(index, col + 1)
+                    hasSpawned = True
+                    dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
                     break
-            
-            
+                elif not check_space(index, col - 1):
+                    spawn(index, col - 1)
+                    hasSpawned = True
+                    dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
+                    break
 
+        if not hasSpawned:
+            for _ in range(board_size):
+                if not check_space(index, 7):
+                    if not check_space(index, 8):
+                        spawn(index, 8)
+                        dlog('Spawned unit at: (' + str(index) + ', ' + str(8) + ')')
+                    spawn(index, 7)
+                    dlog('Spawned unit at: (' + str(index) + ', ' + str(7) + ')')
+                else:
+                    i = random.randint(0, board_size - 1)
+                    if not check_space(index, i):
+                        spawn(index, i)
+                        dlog('Spawned unit at: (' + str(index) + ', ' + str(i) + ')')
+                        break
+
+
+    # defensive moves
+    # if there is someone in the third to bottom row, spawn a defensive pawn on either the left or the right
     bytecode = get_bytecode()
     dlog('Done! Bytecode left: ' + str(bytecode))
-
+    dlog('Round ' + turn + ' end')
