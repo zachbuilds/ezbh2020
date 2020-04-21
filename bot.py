d68e0d5934385119e26b9ede02972d72262d5225
Zach Vincent <vincent_z@delbarton.org>
2020-04-21 15:37:45 -0400

    added control booleans for debugging and improved defensive spawning

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import random
 # This is an example bot written by the developers!
 # Use this to help write your own code, or run it against your bot to see how well you can do!
 
+# cd Python/battlehack20-scaffold-master
+# python run.py examplefuncsplayer C:/Users/exzac/Python/bh2020/ezbh2020 --raw-text
+
 DEBUG = 1
 def dlog(str):
     if DEBUG > 0:
@@ -23,6 +26,12 @@ def check_space_wrapper(r, c, board_size):
         return check_space(r, c)
     except:
         return None
+
+def on_board(targetRow, targetCol):
+    if not targetRow < 0 and not targetRow > 15 and not targetCol < 0 and not targetCol > 15:
+        return True
+    else:
+        return False
 
 
 
@@ -51,30 +60,80 @@ def turn():
 
         if team == Team.WHITE:
             forward = 1
+            backward = -1
         else:
             forward = -1
+            backward = 1
+
+        tryCapture = True
+        avoidCapture = True
+
+
+        # One-stop status checker
+        if (on_board(row + backward, col + 1)):
+            if (check_space(row + backward, col + 1) == same_team):
+                innerBackRight = True
+        if (on_board(row + backward, col - 1)):
+            if (check_space(row + backward, col - 1) == same_team):
+                innerBackLeft = True
+        if (on_board(row + 2*backward, col + 1)):
+            if (check_space(row + (2*backward), col + 1) == same_team):
+                outerBackRight = True
+        if (on_board(row + 2*backward, col - 1)):
+            if (check_space(row + (2*backward), col - 1) == same_team):
+                outerBackLeft = True
+        if (on_board(row + 2*backward, col)):
+            if (check_space(row + (2*backward), col) == same_team):
+                outerBackCenter = True
+
 
 
         # try capturing pieces
-        if check_space_wrapper(row + forward, col + 1, board_size) == opp_team: # up and right
-            capture(row + forward, col + 1)
-            dlog('Captured at: (' + str(row + forward) + ', ' + str(col + 1) + ')')
+        
+        if(tryCapture):
+            if check_space_wrapper(row + forward, col + 1, board_size) == opp_team: # up and right
+                capture(row + forward, col + 1)
+                dlog('Captured at: (' + str(row + forward) + ', ' + str(col + 1) + ')')
+                return
 
-        elif check_space_wrapper(row + forward, col - 1, board_size) == opp_team: # up and left
-            capture(row + forward, col - 1)
-            dlog('Captured at: (' + str(row + forward) + ', ' + str(col - 1) + ')')
+            elif check_space_wrapper(row + forward, col - 1, board_size) == opp_team: # up and left
+                capture(row + forward, col - 1)
+                dlog('Captured at: (' + str(row + forward) + ', ' + str(col - 1) + ')')
+                return
 
         #avoid capture 
-        #if it senses an enemy pawn in the top right or left, don't move. 
-        if (check_space(row - 2, col + 1) == opp_team or check_space(row - 2, col - 1) == opp_team):
-            #do nothing
-            return
-            dlog('Waited patiently')
-        # otherwise try to move forward
-        elif row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size):
-            #               ^  not off the board    ^            and    ^ directly forward is empty
-            move_forward()
-            dlog('Moved forward!')
+        #if it senses an enemy pawn in the top right or left, don't move.
+        if(avoidCapture):
+
+            if (on_board(row + (2*forward), col + 1)):
+                if (check_space(row + (2*forward), col + 1) == opp_team):
+                    if (check_space(row + forward, col + 1) == same_team):
+                        move_forward()
+                        dlog('Would\'ve waited, but had to sacrifice to avoid stalemate')
+                        return
+                    else:
+                        dlog('Waited patiently')
+                        return
+                elif (row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size)):
+                    move_forward()
+            elif (on_board(row + (2*forward), col - 1)):
+                if (check_space(row + (2*forward), col - 1) == opp_team):
+                    if (check_space(row + forward, col - 1) == same_team):
+                        move_forward()
+                        dlog('Would\'ve waited, but had to sacrifice to avoid stalemate')
+                        return
+                    else:
+                        dlog('Waited patiently')
+                        return
+                elif (row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size)):
+                    move_forward()
+            # otherwise try to move forward
+            elif (row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size)):
+                move_forward()
+
+        elif (row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size)):
+                move_forward()
+
 
 
         
@@ -87,43 +146,51 @@ def turn():
         # added this boolean so we don't waste bytecode on spawn loops if something's already been spawned
         hasSpawned = False
 
+        # critRow refers to the critical row that the overlord checks to see if there are any enemies in order to spawn defensive pawns
         if team == Team.WHITE:
             index = 0
+            critRow = 2
         else:
             index = board_size - 1
+            critRow = board_size - 3
 
+        spawnDefense = True
+        spawnRandom = True
+
+        # TODO: SPAWN UNDER ENEMY PAWN IN SECOND TO LAST ROW AS LAST RESORT, MAYBE AVOID SPAWN ON EITHER SIDE?
+        #       PREEMPTIVELY ATTACK ROWS OF TWO
         # checks the third row from the bottom to see if there are enemy pawns and spawns defensive pawn on adjacent column
-        for col in range(0, 16):
-            if check_space(13, col) == opp_team:
-                if not check_space(index, col + 1):
-                    spawn(index, col + 1)
-                    hasSpawned = True
-                    dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
-                    break
-                elif not check_space(index, col - 1):
-                    spawn(index, col - 1)
-                    hasSpawned = True
-                    dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
-                    break
+        if(spawnDefense):
+            for col in range(0, 16):
+                if check_space(critRow, col) == opp_team:
+                    if(on_board(critRow, col + 1)):
+                        if not check_space(index, col + 1):
+                            spawn(index, col + 1)
+                            hasSpawned = True
+                            dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
+                            break
+                    elif(on_board(critRow, col - 1)):
+                        if not check_space(index, col - 1):
+                            spawn(index, col - 1)
+                            hasSpawned = True
+                            dlog('Spawned defensive pawn at ' + str(index) + ', ' + str(col))
+                            break
 
-        if not hasSpawned:
-            for _ in range(board_size):
-                if not check_space(index, 7):
-                    if not check_space(index, 8):
-                        spawn(index, 8)
-                        dlog('Spawned unit at: (' + str(index) + ', ' + str(8) + ')')
-                    spawn(index, 7)
-                    dlog('Spawned unit at: (' + str(index) + ', ' + str(7) + ')')
-                else:
+        if(spawnRandom):
+            if not hasSpawned:
+                for _ in range(board_size):
+    #                if not check_space(index, 7):
+    #                    if not check_space(index, 8):
+    #                        spawn(index, 8)
+    #                        dlog('Spawned unit at: (' + str(index) + ', ' + str(8) + ')')
+    #                    spawn(index, 7)
+    #                    dlog('Spawned unit at: (' + str(index) + ', ' + str(7) + ')')
+    #                else:
                     i = random.randint(0, board_size - 1)
                     if not check_space(index, i):
                         spawn(index, i)
                         dlog('Spawned unit at: (' + str(index) + ', ' + str(i) + ')')
                         break
 
-
-    # defensive moves
-    # if there is someone in the third to bottom row, spawn a defensive pawn on either the left or the right
     bytecode = get_bytecode()
     dlog('Done! Bytecode left: ' + str(bytecode))
-    dlog('Round ' + turn + ' end')
